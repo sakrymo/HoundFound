9aef9e86609e4c0eba5dcf144ff363544cc33abe
Sakrymo <kr.moskal@email.com>
2021-10-27 00:23:32 +0200

    -

--- script.js
+++ script.js
@@ -1,5 +1,87 @@
-// TODO: 
+// const { tween } = popmotion
+
+// console.log(tween)
+// const counter = document.querySelector('.counter')
+// const updateCounter = v => (counter.innerHTML = v)
+
+// tween().start(updateCounter)
+
+// import { animate } from 'popmotion'
+
+// const $root = document.querySelector('body')
+
+// const from = -5
+// const to = 5
+// const duration = 10000
+// const type = 'spring'
+// const bounce = 1
+// const velocity = 1
+
+// animate({
+//   from: from,
+//   to: to,
+//   duration: duration,
+//   type: type,
+//   bounce: bounce,
+//   velocity: velocity,
+//   onUpdate: latest =>
+//   {
+//     $root.innerHTML = `from: ${from}<br>to ${to}<br>latest: ${latest}`
+//     $root.innerHTML += `<div class="circle" style="transform: translateY(${Math.sin(latest) * 100 + 10 * latest}px) translateX(-${Math.cos(latest) * 100 + 10 * latest}px)"></div>`
+//   }
+// })
+const PIXABAY_API_KEY = '9324111-d9394a0e56f6eb524da75f703'
+const PIXABAY_RANDOM_DOG_URL = `https://pixabay.com/api/?key=${PIXABAY_API_KEY}&q=dog&image_type=photo&orientation=horizontal&per_page=200&editors_choice=true`
+const DOG_API_RANDOM_URL = 'https://dog.ceo/api/breeds/image/random'
+
+fetch(DOG_API_RANDOM_URL)
+  .then(response => {
+    const responseJSON = response.json()
+    return responseJSON
+  })
+  .then(responseJSON => {
+    console.log(responseJSON.message)
+  })
+
+// Set background image
+const bgImg = document.querySelector('.bg-img-container')
+
+fetch(PIXABAY_RANDOM_DOG_URL)
+  .then(response => {
+    const responseJSON = response.json()
+    return responseJSON
+  })
+  .then(responseJSON => {
+    const dogIndex = Math.floor(Math.random() * responseJSON.hits.length)
+    let dog = responseJSON.hits[dogIndex]
+    let url = dog.largeImageURL
+    
+    console.log(responseJSON)
+    // const img = document.createElement('img')
+    // img.setAttribute('src', url)
+    // img.setAttribute('alt', dog.tags)
+    // bgImgParent.appendChild(img)
+    bgImg.style.backgroundImage = `url(${url})`
+  })
+
+/*
+888b. 888b. 8888 8    .d88b.    db    888b. 8888 888b.
+8  .8 8  .8 8www 8    8P  Y8   dPYb   8   8 8www 8  .8
+8wwP' 8wwK' 8    8    8b  d8  dPwwYb  8   8 8    8wwK'
+8     8  Yb 8888 8888 `Y88P' dP    Yb 888P' 8888 8  Yb
+*/
+
+const preloader = document.querySelector('.preloader')
+
+/*
+888b.    db    .d88b 8  dP .d88b  888b. .d88b. 8    8 8b  8 888b.
+8wwwP   dPYb   8P    8wdP  8P www 8  .8 8P  Y8 8    8 8Ybm8 8   8
+8   b  dPwwYb  8b    88Yb  8b  d8 8wwK' 8b  d8 8b..d8 8  "8 8   8
+888P' dP    Yb `Y88P 8  Yb `Y88P' 8  Yb `Y88P' `Y88P' 8   8 888P'
+*/
+
+// TODO:
 // use ajax for dog api
 // add a select menu for the breed
+// make breed searchbar into text input with autosuggest + dropwdown checkboxes
 // use popmotion for some fancy animation maybe for loading?
- 
